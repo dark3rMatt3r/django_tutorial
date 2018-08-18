eabbf0016b01762dd4e44c343a878798a6b5d9dc
nHansberry <grrr1981@gmail.com>
2018-08-18 15:10:55 -0500

    Basic view now returns minial HTML

--- functional_tests.py
+++ functional_tests.py
@@ -9,7 +9,7 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def test_can_start_a_list_ad_retrieve_it_later(self):
+    def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
         self.assertIn('To-Do', self.browser.title)
         self.fail('Finish the test!')
