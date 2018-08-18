7164a15e9bc4140db4e2f24cda49f33fb60bd1ec
nHansberry <grrr1981@gmail.com>
2018-08-17 22:31:26 -0500

    second functional test

--- functional_tests.py
+++ functional_tests.py
@@ -1,9 +1,18 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
+class NewVisitorTest(unittest.TestCase):
 
-browser.get('http://localhost:8000')
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
-assert 'To-Do' in browser.title
+    def tearDown(self):
+        self.browser.quit()
 
-browser.quit()
+    def test_can_start_a_list_ad_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
